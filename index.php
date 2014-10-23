3a1a1f548ae2146ed4ad347cd98afae9b24a4255
jan.mattner <spam@mail.com>
2014-10-23 20:01:59 +0200

    Update index for current directory structure

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-require_once '/xataface/dataface-public-api.php';
-df_init(__FILE__, '/xataface')->display();
+require_once '../xataface/dataface-public-api.php';
+df_init(__FILE__, '../xataface')->display();
